680cd5b5af5b92b9f01a225efc37e1022c663f25
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2022-12-22 09:32:51 +0530

    empty default list

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-let items = ["Buy Groceries", "Cook Food"];
+let items = [];
 let workItems = [];
 
 app.get("/",(req,res)=>{
